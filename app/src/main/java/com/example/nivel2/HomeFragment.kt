32319c3b592a92f5ada9b0f1e85fb3042adb1330
CarMunguia <carmunguiaa16@gmail.com>
2022-06-06 14:28:50 -0500

    committt

---
+++ app/src/main/java/com/example/nivel2/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.example.nivel2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.example.nivel2.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+
+        var navController = findNavController()
+
+
+        return binding.root
+    }
+}
